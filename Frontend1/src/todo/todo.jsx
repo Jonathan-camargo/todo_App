e302810832b0ad2a89f5da25c5a583e71d6c9fa8
Jonathan Camargo <camargo153@hotmail.com>
2022-08-16 17:25:19 -0300

    a

--- Frontend1/src/todo/todo.jsx
+++ Frontend1/src/todo/todo.jsx
@@ -4,11 +4,15 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    handleAdd() {
+
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Taregas' small='Cadastro'></PageHeader>
-                <TodoForm></TodoForm>
+                <TodoForm handleAdd={this.handleAdd}></TodoForm>
                 <TodoList></TodoList>
             </div>
         )
