a80c20590e12200d9a5b66e0bf8f363eacbaa8c0
Jonathan Camargo <camargo153@hotmail.com>
2022-08-16 16:13:43 -0300

    a

---
+++ Frontend1/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import pageHeader from "../template/pageHeader";
+
+export default props => (
+    <div>
+        <pageHeader name='Sobre' small='Nós'></pageHeader>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos expedita incidunt esse dolorum porro debitis atque. Officia, vel dolores porro ad eaque harum dolore quam excepturi vitae similique, libero esse?</p>
+        <h2>Missão e visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis quisquam repellat temporibus iure corrupti illum reiciendis perspiciatis fugiat inventore est impedit eveniet, blanditiis aliquid, repudiandae saepe libero ullam! Quasi, ullam?</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam eveniet quasi non quis eius magni? Numquam, voluptatem excepturi quia sequi reiciendis vero velit optio nemo assumenda hic enim ducimus fugit.</p>
+    </div>
+)
