f6fc96139bab55cc82d214c7f0a9f1d4c754e04a
Jonathan Camargo <camargo153@hotmail.com>
2022-08-17 09:03:55 -0300

    a

--- Frontend1/src/todo/todo.jsx
+++ Frontend1/src/todo/todo.jsx
@@ -1,19 +1,32 @@
 import React, { Component } from 'react'
-import PageHeader from '../template/pageHeader'
-import TodoForm from './todoForm'
-import TodoList from './todoList'
+
+import PageHeader from '../template/PageHeader'
+import TodoForm from './TodoForm'
+import TodoList from './TodoList'
 
 export default class Todo extends Component {
-    handleAdd() {
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
 
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
     }
 
-    render() {
-        return (
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value})
+    }
+
+    handleAdd() {
+        console.log(this.state.description)
+    }
+    
+    render(){
+        return(
             <div>
-                <PageHeader name='Taregas' small='Cadastro'></PageHeader>
-                <TodoForm handleAdd={this.handleAdd}></TodoForm>
-                <TodoList></TodoList>
+                <PageHeader name="Tarefas" small="Cadastro"/>
+                <TodoForm description={this.state.description} handleChange={this.handleChange} handleAdd={this.handleAdd}/>
+                <TodoList />
             </div>
         )
     }
